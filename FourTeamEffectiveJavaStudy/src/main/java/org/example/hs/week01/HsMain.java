477e309aaad70d18fc72bf57559a09bf07c92209
mesung <myhs2002@gmail.com>
2020-07-16 01:05:12 +0900

    item 1 test source

---
+++ FourTeamEffectiveJavaStudy/src/main/java/org/example/hs/week01/HsMain.java
@@ -0,0 +1,11 @@
+package org.example.hs.week01;
+
+import java.util.EnumSet;
+
+public class HsMain {
+    public static void main(String[] args) {
+        Boolean.valueOf(true);
+        Boolean b = new Boolean(true);
+        EnumSet<EnumTest> enumSetTest;
+    }
+}
