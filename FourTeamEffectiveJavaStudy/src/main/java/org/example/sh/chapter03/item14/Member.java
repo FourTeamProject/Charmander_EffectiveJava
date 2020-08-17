09fc6a4d8a17d0dd1bfa31ae6c90bebf107cf34a
bbubbush <bbubbush@gmail.com>
2020-08-17 23:27:57 +0900

    잘못된 샘플코드 수정

--- FourTeamEffectiveJavaStudy/src/main/java/org/example/sh/chapter03/item14/Member.java
+++ FourTeamEffectiveJavaStudy/src/main/java/org/example/sh/chapter03/item14/Member.java
@@ -2,15 +2,35 @@ package org.example.sh.chapter03.item14;
 
 
 public class Member implements Comparable<Member> {
-    private Long id;
-    private String name;
-    private Long age;
+    private final Long id;
+    private final String name;
+    private final Long age;
 
     public Member(Long id, String name, Long age) {
         this.id = id;
         this.name = name;
         this.age = age;
     }
+    @Override
+    public int compareTo(Member o) {
+        return orderById(o);
+    }
+    private int orderById(Member o) {
+        Long memberId = this.getId();
+        Long targetId = o.getId();
+        return memberId > targetId ? 1 : ((memberId == targetId) ? 0 : -1);
+    }
+    private int orderByName(Member o) {
+        String memberName = this.getName();
+        String targetName = o.getName();
+        return memberName.charAt(0) > targetName.charAt(0) ? 1 : ((memberName.charAt(0) == targetName.charAt(0)) ? 0 : -1);
+    }
+    private int orderByAge(Member o) {
+        Long memberAge = this.getAge();
+        Long targetAge = o.getAge();
+        return memberAge > targetAge ? 1 : ((memberAge == targetAge) ? 0 : -1);
+    }
+
 
     public Long getId() {
         return id;
@@ -23,32 +43,6 @@ public class Member implements Comparable<Member> {
     public Long getAge() {
         return age;
     }
-
-    private int sortById(Member o) {
-        Long memberId = this.getId();
-        Long targetId = o.getId();
-        return memberId < targetId ? 1 : ((memberId == targetId) ? 0 : -1);
-    }
-    private int sortByName(Member o) {
-        String memberName = this.getName();
-        String targetName = o.getName();
-
-        return memberName.charAt(0) < targetName.charAt(0) ? 1 : ((memberName.charAt(0) == targetName.charAt(0)) ? 0 : -1);
-    }
-
-    private int sortByAge(Member o) {
-        Long memberAge = this.getAge();
-        Long targetAge = o.getAge();
-
-        return memberAge < targetAge ? 1 : ((memberAge == targetAge) ? 0 : -1);
-    }
-
-    @Override
-    public int compareTo(Member o) {
-        return sortById(o);
-    }
-
-
     @Override
     public String toString() {
         return "Member{" +
